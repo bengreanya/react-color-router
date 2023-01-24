791618d0527bcb815b827bbf176f75f0c367ab2d
Ben Greanya <bengreanya@gmail.com>
2023-01-24 15:56:47 -0800

    styled notfound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import './NotFound.css';
 
 export default function NotFound() {
-  return <h1>NotFound</h1>;
+  return (
+    <div className="background2">
+      <h2>NotFound!!!</h2>
+      <p>Though this is rgb(255, 106, 0)</p>
+    </div>
+  );
 }
